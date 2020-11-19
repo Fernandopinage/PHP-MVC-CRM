95dfb0022826044901e7feba354dd0638da383c9
LUIZ COUTINHO <LUIZ.COUTINHO@konec.local>
2020-11-19 17:09:54 -0400

    botao com modal

--- php/colaborador.php
+++ php/colaborador.php
@@ -117,8 +117,29 @@
             <div class="input-group mb-3 form-group col-md-3">
                 <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
                 <div class="btn-group-toggle" data-toggle="buttons">
-                    
-                    <input type="button" class="btn btn-dark" value="♣">                   
+                    <button type="button" class="btn btn-dark" data-toggle="modal" data-target="#exampleModal">
+                        ♣
+                    </button>
+                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    ...
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary">Save changes</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
 
