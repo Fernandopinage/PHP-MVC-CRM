7960910a2c62d3523eb7cc0508a52857d0d86be3
LUIZ COUTINHO <LUIZ.COUTINHO@konec.local>
2020-11-23 17:00:54 -0400

    ok

--- php/calendario.php
+++ php/calendario.php
@@ -9,34 +9,12 @@ include_once "../layout/head.php";
     var initialLocaleCode = 'pt-br';
     var localeSelectorEl = document.getElementById('locale-selector');
     var calendarEl = document.getElementById('calendar');
-
     var calendar = new FullCalendar.Calendar(calendarEl, {
       headerToolbar: {
         left: 'prev,next today',
         center: 'title',
         right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
       },
-
-      // inicio da alteração
-          eventClick: function(info) {
-            //alert('Event: ' + info.event.title);
-            //alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
-            //alert('View: ' + info.view.type);
-
-            // change the border color just for fun
-            info.el.style.borderColor = 'red';
-          },
-
-          dateClick: function(info) {
-          alert('clicked ' + info.dateStr);
-          },
-          select: function(info) {
-            $('#myModal').modal('show');
-            
-          },
-      // aqui final da alteração
-
-      
       //initialDate: '2020-09-12',
       locale: initialLocaleCode,
       buttonIcons: false, // show the prev/next text
@@ -44,14 +22,14 @@ include_once "../layout/head.php";
       navLinks: true, // can click day/week names to navigate views
       editable: true,
       dayMaxEvents: true, // allow "more" link when too many events
-      events: [{
-        title: 'All Day Event',
-        start: '2020-09-01'
-      }]
+      events: [
+        {
+          title: 'All Day Event',
+          start: '2020-09-01'
+        }
+      ]
     });
-
     calendar.render();
-
     // build the locale selector's options
     calendar.getAvailableLocaleCodes().forEach(function(localeCode) {
       var optionEl = document.createElement('option');
@@ -60,14 +38,12 @@ include_once "../layout/head.php";
       optionEl.innerText = localeCode;
       localeSelectorEl.appendChild(optionEl);
     });
-
     // when the selected option changes, dynamically change the calendar option
     localeSelectorEl.addEventListener('change', function() {
       if (this.value) {
         calendar.setOption('locale', this.value);
       }
     });
-
   });
 </script>
 </head>
