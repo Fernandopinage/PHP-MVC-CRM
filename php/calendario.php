17f7fa2a1db1d58710f2351bbcf677e325630e91
LUIZ COUTINHO <LUIZ.COUTINHO@konec.local>
2020-11-24 09:59:39 -0400

    função modal no calendario

--- php/calendario.php
+++ php/calendario.php
@@ -29,7 +29,11 @@ include_once "../layout/head.php";
       }],
       eventClick: function(info) {
         $('#visualizar').modal('show');
-      }
+      },selectable: true,
+    select: function(info) {
+      //alert('inicio ' + info.start.toLocaleString());
+      $('#long').modal('show');
+    }
     });
 
     calendar.render();
@@ -77,6 +81,34 @@ include_once "../layout/head.php";
   </div>
 </div>
 
+<div class="modal fade" id="long" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">New message</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="form-group">
+            <label for="recipient-name" class="col-form-label">Recipient:</label>
+            <input type="text" class="form-control" id="recipient-name">
+          </div>
+          <div class="form-group">
+            <label for="message-text" class="col-form-label">Message:</label>
+            <textarea class="form-control" id="message-text"></textarea>
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Send message</button>
+      </div>
+    </div>
+  </div>
+</div>
 
 <?php
 
