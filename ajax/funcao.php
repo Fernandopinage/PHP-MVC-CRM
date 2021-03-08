57e589fff55b99a0d6f3f9a8a1e97ac3776c372e
FernandoPinage <luizfernandoluck@hotmail.com>
2021-03-08 17:32:28 -0400

    ok

--- ajax/funcao.php
+++ ajax/funcao.php
@@ -4,17 +4,17 @@ include_once "../class/classGrupoUsuario.php";
 include_once "../dao/GrupoUsuarioDAO.php";
 
 
-
 $cod = $_POST['codigo'];
 $sigle = $_POST['sigla'];
 $descricao = $_POST['descricao'];
 
-$GrupoUsuario = new ClassGrupoUsuario();
-$GrupoUsuario->setCod($cod);
-$GrupoUsuario->setCod($sigle);
-$GrupoUsuario->setCod($descricao);
+$ClassGrupoUsuario = new ClassGrupoUsuario();
 
-$grupo = new ClassGrupoUsuarioDAO();
-$grupo->insertGrupoUsuario($GrupoUsuario);
+$ClassGrupoUsuario->setCod($cod);
+$ClassGrupoUsuario->setSigla($sigle);
+$ClassGrupoUsuario->setDesc($descricao);
+
+$GrupoUsuario = new ClassGrupoUsuarioDAO();
+$GrupoUsuario->insertGrupoUsuario($ClassGrupoUsuario);
 
 ?>
--- dao/GrupoUsuarioDAO.php
+++ dao/GrupoUsuarioDAO.php
@@ -7,18 +7,19 @@ include_once "../class/classGrupoUsuario.php";
     Class ClassGrupoUsuarioDAO extends Dao{
 
 
-        public function insertGrupoUsuario(GrupoUsuario $GrupoUsuario){
+        public function insertGrupoUsuario(ClassGrupoUsuario $ClassGrupoUsuario){
 
             $sql = "INSERT INTO `crm_gru`(`CRM_GRU_ID`, `CRM_GRU_COD`, `CRM_GRU_DESC`, `CRM_GRU_SIGLA`, `CRM_GRU_IDEMP`, `CRM_GRU_COMPLEMENTO`, `CRM_GRU_BLOQ`, `CRM_GRU_EXCLUIDO`) VALUES (null, :cod, :descr, :sigla, ``, ``, ``, ``)";
             $insert = $this->con->prepare($sql);
-            $insert->bindValue('cod',$GrupoUsuario->getCod());
-            $insert->bindValue('descr',$GrupoUsuario->getDesc());
-            $insert->bindValue('sigla',$GrupoUsuario->getSigla());
+            $insert->bindValue(':cod',$ClassGrupoUsuario->getCod());
+            $insert->bindValue(':descr',$ClassGrupoUsuario->getDesc());
+            $insert->bindValue(':sigla',$ClassGrupoUsuario->getSigla());
+            
            // $insert->bindValue('cod',$GrupoUsuario->);
            // $insert->bindValue('cod',$GrupoUsuario->);
            // $insert->bindValue('cod',$GrupoUsuario->);
            // $insert->bindValue('cod',$GrupoUsuario->);
-           $insert->execute();
+            $insert->execute();
         }
         
     }
